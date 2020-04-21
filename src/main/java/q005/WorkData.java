5f83ea2f5323ed0232881be0e162fc3458605f74
BuildTools <unconfigured@null.spigotmc.org>
2020-04-21 15:48:38 +0900

    Q005実施

--- src/main/java/q005/WorkData.java
+++ src/main/java/q005/WorkData.java
@@ -19,4 +19,32 @@ public class WorkData {
 
     /** 作業時間(分) */
     private int workTime;
+
+    WorkData(String number, String department, String position, String pCode, int workTime){
+        this.number = number;
+        this.department = department;
+        this.position = position;
+        this.pCode = pCode;
+        this.workTime = workTime;
+    }
+
+    public String getNumber(){
+        return this.number;
+    }
+
+    public String getDepartment(){
+        return this.department;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getPCode() {
+        return pCode;
+    }
+
+    public int getWorkTime() {
+        return workTime;
+    }
 }
